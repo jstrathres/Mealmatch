df9e948a04a7eabe88ac308b2dbb0878d75cc3ff
John Glandon <glandoncomputer@Johns-iMac.local>
2023-03-16 09:57:08 -0400

    Added recipe controller

--- GroupFinalProject/ClientApp/proxy.conf.js
+++ GroupFinalProject/ClientApp/proxy.conf.js
@@ -7,6 +7,7 @@ const PROXY_CONFIG = [
   {
     context: [
       "/weatherforecast",
+      "/api/Recipe"
    ],
     target: target,
     secure: false,
---
+++ GroupFinalProject/Controllers/RecipeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupFinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GroupFinalProject.Controllers
+{
+    [Route("api/[controller]")]
+    public class RecipeController : Controller
+    {
+        MealmatchContext context = new MealmatchContext();
+
+        [HttpGet]
+        public List<Recipe> getRecipes()
+        {
+            return context.Recipes.ToList();
+        }
+
+        [HttpGet("{userid}")]
+        public List<Recipe> getRecipeByUserId(int userid)
+        {
+            return context.Recipes.Where(r => r.UserId == userid).ToList();
+        }
+
+        [HttpPost]
+        public Recipe AddRecipe(int recipeid, int userid)
+        {
+            Recipe newRecipe = new Recipe()
+            {
+                RecipeId = recipeid,
+                UserId = userid
+            };
+            context.Recipes.Add(newRecipe);
+            context.SaveChanges();
+            return newRecipe;
+        }
+
+        [HttpDelete]
+        public Recipe DeleteRecipe(int id)
+        {
+            Recipe recipe = context.Recipes.FirstOrDefault(r => r.Id == id);
+            context.Recipes.Remove(recipe);
+            context.SaveChanges();
+            return recipe;
+        }
+    }
+}
+
