b7cea46701adc1cf29c6db6fcaed062c5f00b0e3
jstrathres <jstraz7@gmail.com>
2023-03-30 10:24:47 -0400

    added google buttons, cleaned up some code

--- GroupFinalProject/ClientApp/src/app/home/home.component.ts
+++ GroupFinalProject/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(private authService: SocialAuthService){}
+
+  user: SocialUser = {} as SocialUser;
+  loggedIn: boolean = false;
+
+  ngOnInit(): void {
+
+    this.authService.authState.subscribe((user) => {
+  	  this.user = user;
+  	  this.loggedIn = (user != null);
+      console.log(this.user);
+      //add favorites.getfavorites to only show favorites when logged in
+      //set userId to nvarcahr(max) in DB to account for long usernames
+    });
+  }
 }
